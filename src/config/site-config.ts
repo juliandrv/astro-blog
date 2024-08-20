a291905c2fd2778d29f2fc2e70abdec97b841131
juliandrv.dev <juliandrv.dev@gmail.com>
2024-08-20 15:47:04 +0000

    Astro Blog

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: "Julián's Blog",
+  description: "Blog oficial de Julián Ramírez - Frontend Developer"
+}
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: image().refine(img => img.width < 1500, {
+      message: 'Image should be lower than 1500px'
+    }),
+    // Relación
+    author: z.string(),
+    // Relación
+    tags: z.array(z.string()),
+  })
+})
+
+export const collections = {
+  "blog": blogCollection
+}
