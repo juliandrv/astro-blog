0c9d4fa1feb79c39f9fd2148428a43d79a5f6fdd
juliandrv.dev <juliandrv.dev@gmail.com>
2024-08-23 16:19:23 +0000

    Added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  image: {
-    service: squooshImageService(),
-  },
+	integrations: [tailwind(), mdx()],
+	image: {
+		service: squooshImageService(),
+	},
+	site: "https://example.com",
 });
